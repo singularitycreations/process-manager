4867a76fded9ba07877d5e426349bcf4fbba5949
Eignart <eignart@scr.ee>
2019-03-24 16:20:16 +0200

    Default vaule fix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,13 @@
-module.exports = { "extends": "airbnb-base" };
+module.exports = { 
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": 0,
+        "no-console": 'off',
+        "max-len": [
+            "error", { 
+                "ignoreComments": true,
+                "code": 100 
+            }
+        ]
+    }    
+ };

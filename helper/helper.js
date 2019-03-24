223830b73d6ec62ca49790afd2d26c56edd86352
Eignart <eignart@scr.ee>
2019-03-24 16:28:03 +0200

    New helper functions

--- helper/helper.js
+++ helper/helper.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const fs = require('fs');
 
 /**
  * Get formated date.
@@ -20,4 +21,36 @@ exports.getDate = function formatedDate({ format = 'Y-MM-DD HH:mm:ss' } = {}) {
  */
 exports.timeout = function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+/**
+ * Simple file read wrapper.
+ *
+ * @param {String} filePath Path to file.
+ * @returns {String | Boolean} If file read failed (probably file not exist) then returns false
+ *  otherwise file content.
+ * @author Eignart
+ * @since 2019.03.24
+ */
+exports.readFile = function readFile(filePath) {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
+  } catch (error) {
+    // file not exist
+    return false;
+  }
+};
+
+/**
+ * The hasProperty() method returns a boolean indicating whether the object has the specified
+ * property as its own property (as opposed to inheriting it).
+ *
+ * @param {Object} obj Object where to check.
+ * @param {String} key Object key name to check.
+ * @returns {Boolean} true | false
+ * @author Eignart
+ * @since 2019.03.24
+ */
+exports.hasProperty = function hasProperty(obj, key) {
+  return Object.prototype.hasOwnProperty.call(obj, key);
 };
